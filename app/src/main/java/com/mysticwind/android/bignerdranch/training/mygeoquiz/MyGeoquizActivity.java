e3e7a2d5d14cc151b8c280e358d03678b7770f25
Rick Wang <wangziwe@amazon.com>
2016-08-15 13:01:23 -0700

    MyGeoquizActivity: add logs for activity lifecycle

--- app/src/main/java/com/mysticwind/android/bignerdranch/training/mygeoquiz/MyGeoquizActivity.java
+++ app/src/main/java/com/mysticwind/android/bignerdranch/training/mygeoquiz/MyGeoquizActivity.java
@@ -2,6 +2,7 @@ package com.mysticwind.android.bignerdranch.training.mygeoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -13,6 +14,8 @@ import com.mysticwind.android.bignerdranch.training.mygeoquiz.manager.QuizManage
 import com.mysticwind.android.bignerdranch.training.mygeoquiz.model.Question;
 
 public class MyGeoquizActivity extends AppCompatActivity {
+
+    private static final String TAG = MyGeoquizActivity.class.getSimpleName();
 
     private TextView quizTextView;
     private Button yesButton;
@@ -34,6 +37,9 @@ public class MyGeoquizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        debug("onCreate()");
+
         setContentView(R.layout.activity_my_geoquiz);
 
         quizTextView = (TextView) findViewById(R.id.quiz_text_view);
@@ -76,6 +82,36 @@ public class MyGeoquizActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        debug("onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        debug("onDestroy()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        debug("onPause()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        debug("onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        debug("onStart()");
+    }
+
     private void checkAnswerAndShowToast(boolean enteredAnswer) {
         boolean isAnswerCorrect = quizManager.answer(enteredAnswer);
         int stringResourceId = isAnswerCorrect ? R.string.answer_correct : R.string.answer_incorrect;
@@ -88,5 +124,9 @@ public class MyGeoquizActivity extends AppCompatActivity {
         nextButton.setEnabled(enablePreviousAndNextButton);
         yesButton.setEnabled(!enablePreviousAndNextButton);
         noButton.setEnabled(!enablePreviousAndNextButton);
+    }
+
+    private void debug(String message) {
+        Log.d(TAG, message);
     }
 }
