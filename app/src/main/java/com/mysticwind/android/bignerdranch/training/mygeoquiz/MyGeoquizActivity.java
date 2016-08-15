a4bf8447e8447831bde372a5dec03f448abce124
Rick Wang <wangziwe@amazon.com>
2016-08-15 11:15:55 -0700

    Add question bank and rotate the quiz

--- app/src/main/java/com/mysticwind/android/bignerdranch/training/mygeoquiz/MyGeoquizActivity.java
+++ app/src/main/java/com/mysticwind/android/bignerdranch/training/mygeoquiz/MyGeoquizActivity.java
@@ -4,23 +4,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.mysticwind.android.bignerdranch.training.mygeoquiz.model.Question;
 
 public class MyGeoquizActivity extends AppCompatActivity {
 
+    private TextView quizTextView;
     private Button yesButton;
     private Button noButton;
+    private Button nextButton;
+    private int currentIndex = 0;
+
+    private Question[] questions = new Question[] {
+            new Question(R.string.Oceans, true),
+            new Question(R.string.Mideast, false),
+            new Question(R.string.Africa, false),
+            new Question(R.string.Americas, true),
+            new Question(R.string.Asia, true),
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_geoquiz);
 
+        quizTextView = (TextView) findViewById(R.id.quiz_text_view);
+        quizTextView.setText(questions[0].getTextResourceId());
+
         yesButton = (Button) findViewById(R.id.yesButton);
         yesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MyGeoquizActivity.this, "YES!", Toast.LENGTH_SHORT).show();
+                checkAnswerAndShowToast(true);
             }
         });
 
@@ -28,8 +45,23 @@ public class MyGeoquizActivity extends AppCompatActivity {
         noButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MyGeoquizActivity.this, "NO!", Toast.LENGTH_SHORT).show();
+                checkAnswerAndShowToast(false);
             }
         });
+
+        nextButton = (Button) findViewById(R.id.next_button_view);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentIndex = (currentIndex + 1) % questions.length;
+                quizTextView.setText(questions[currentIndex].getTextResourceId());
+            }
+        });
+    }
+
+    private void checkAnswerAndShowToast(boolean enteredAnswer) {
+        boolean isAnswerCorrect = questions[currentIndex].validateAnswer(enteredAnswer);
+        int stringResourceId = isAnswerCorrect ? R.string.answer_correct : R.string.answer_incorrect;
+        Toast.makeText(MyGeoquizActivity.this, stringResourceId, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/mysticwind/android/bignerdranch/training/mygeoquiz/model/Question.java
@@ -0,0 +1,20 @@
+package com.mysticwind.android.bignerdranch.training.mygeoquiz.model;
+
+public class Question {
+
+    private final int textResourceId;
+    private final boolean isAnswerTrue;
+
+    public Question(int textResourceId, boolean isAnswerTrue) {
+        this.textResourceId = textResourceId;
+        this.isAnswerTrue = isAnswerTrue;
+    }
+
+    public int getTextResourceId() {
+        return textResourceId;
+    }
+
+    public boolean validateAnswer(boolean answer) {
+        return answer == isAnswerTrue;
+    }
+}
